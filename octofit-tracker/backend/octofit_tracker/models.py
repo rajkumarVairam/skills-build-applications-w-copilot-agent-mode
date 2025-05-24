c99c13f742d6398c1d413de8b7e47de99da53b71
rajkumarVairam <raj.vrajkumar1987@gmail.com>
2025-05-24 20:46:22 +0000

    fixed error

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,27 +1,32 @@
 from djongo import models
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     class Meta:
+        app_label = 'octofit_tracker'
         abstract = False
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
     members = models.ArrayField(model_container=User)
     class Meta:
+        app_label = 'octofit_tracker'
         abstract = False
 
 class Activity(models.Model):
     name = models.CharField(max_length=255)
     duration = models.IntegerField()
     class Meta:
+        app_label = 'octofit_tracker'
         abstract = False
 
 class Leaderboard(models.Model):
     team = models.EmbeddedField(model_container=Team)
     score = models.IntegerField()
     class Meta:
+        app_label = 'octofit_tracker'
         abstract = False
 
 class Workout(models.Model):
@@ -29,4 +34,5 @@ class Workout(models.Model):
     activity = models.EmbeddedField(model_container=Activity)
     date = models.DateField()
     class Meta:
+        app_label = 'octofit_tracker'
         abstract = False

e98346aeeffefa5ec8739d99991e3d67994064b4
rajkumarVairam <raj.vrajkumar1987@gmail.com>
2025-05-24 20:22:18 +0000

    Step4 completed

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_5+ay=7m#v*re=1e4%%@i^tvyalfgx#mx_d21etud^hbl9b5c-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "supreme-pancake-967gx4vpvqx2xrpq-8000.app.github.dev"]
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django.http import JsonResponse
 from .models import User, Team, Activity, Leaderboard, Workout
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
 
@@ -21,3 +22,6 @@ class LeaderboardViewSet(viewsets.ModelViewSet):
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
+
+def api_root(request):
+    return JsonResponse({"message": "Welcome to the Octofit API!", "url": "https://supreme-pancake-967gx4vpvqx2xrpq-8000.app.github.dev"})
